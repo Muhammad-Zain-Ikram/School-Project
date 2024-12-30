832fab87717886093d62dd99862793b6a81a5c7a
Zain IKram <gamingsage92@gmail.com>
2024-12-30 21:39:37 +0500

    Vite Config changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src', // Allows you to use @ as an alias for /src
+    },
+  },
+  build: {
+    outDir: 'dist', // Ensures the build output directory is 'dist'
+  },
+});
